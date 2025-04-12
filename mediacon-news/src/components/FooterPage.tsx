e3387b5d30af47760d3df811c7c8fe470358578c
ABHISHEK <abhishekp9a@gmail.com>
2025-04-12 07:29:37 +0530

    Converted into next

---
+++ mediacon-news/src/components/FooterPage.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/footer.module.css"
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <h3>MULEARN UCEK 2025</h3>
+    </div>
+  )
+}
---
+++ mediacon-news/types.ts
@@ -0,0 +1,13 @@
+export type Article = {
+    id: string
+    title: string
+    category: string
+    source: string
+    sourceImage?: string
+    author?: string
+    time: string
+    date?: string
+    image: string
+    content: string[]
+  }
+  
