11072d0667810d0370aa0a3b31035ef10f21f021
ABHISHEK <abhishekp9a@gmail.com>
2025-04-13 17:09:55 +0530

    Changed loading screen

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,20 +1,29 @@
-// src/components/Loader.tsx
-"use client"
-import Image from "next/image"
-import styles from "../styles/Loader.module.css"
+"use client";
+import styles from "../styles/Loader.module.css";
+import Image from "next/image";
 
-export default function Loader() {
+export default function LoadingScreen() {
   return (
     <div className={styles.loaderContainer}>
-      <div className={styles.glowAnimation}>
+      <div className={styles.logoWrapper}>
         <Image
-          src="/assets/logo.png"
-          alt="Mediacon Logo"
-          width={300}
-          height={300}
-          priority
+          src="/assets/Con(Logo).png"
+          alt="Mediacon Mike"
+          width={250}
+          height={250}
         />
+        <svg
+          className={styles.animatedStroke}
+          viewBox="0 0 500 800"
+          xmlns="http://www.w3.org/2000/svg"
+          
+        >
+          <path
+            d="M245,10c130,0,235,105,235,235S375,480,245,480S10,375,10,245S115,10,245,10Z"
+            className={styles.strokePath}
+          />
+        </svg>
       </div>
     </div>
-  )
+  );
 }
