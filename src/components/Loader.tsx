2b934e77d405914998147a271bc78268a16d3685
ABHISHEK <abhishekp9a@gmail.com>
2025-04-13 23:54:18 +0530

    Fixed loading device bug

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -6,22 +6,26 @@ export default function LoadingScreen() {
   return (
     <div className={styles.loaderContainer}>
       <div className={styles.logoWrapper}>
-        <Image
-          src="/assets/Con(Logo).png"
-          alt="Mediacon Mike"
-          width={250}
-          height={250}
-        />
+        <div className={styles.logoInner}>
+          <Image
+            src="/assets/Con(Logo).png"
+            alt="Mediacon Logo"
+            fill
+            className={styles.logoImage}
+          />
+        </div>
         <svg
           className={styles.animatedStroke}
-          viewBox="0 0 500 800"
+          viewBox="0 0 250 250"
           xmlns="http://www.w3.org/2000/svg"
-          
         >
-          <path
-            d="M245,10c130,0,235,105,235,235S375,480,245,480S10,375,10,245S115,10,245,10Z"
+          <circle
+            cx="125"
+            cy="130"
+            r="100"
             className={styles.strokePath}
           />
+          
         </svg>
       </div>
     </div>
