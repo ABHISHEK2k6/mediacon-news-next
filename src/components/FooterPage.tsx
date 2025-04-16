da34a938e58425bbd1c0aaeff3e4c398b43f2213
ABHISHEK2k6 <abhishekp9a@gmail.com>
2025-04-16 09:57:42 +0530

    Update FooterPage.tsx

--- src/components/FooterPage.tsx
+++ src/components/FooterPage.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/footer.module.css"
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <h3>M© 2025 <a href="https://mediacon.ctlg.in/">MediaCon. All </a> rights reserved.</h3>
+      <h3>M© 2025 <a href="https://mediacon.ctlg.in/">MediaCon.</a> All rights reserved.</h3>
     </div>
   )
 }
